ec9f66e15e5bead9373ead236d836149eed38b89
benfild <fildben@gmail.com>
2021-07-24 20:06:51 -0400

    adding grid component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 //components
 import HeroImage from './HeroImage';
+import Grid from './Grid';
 
 //Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -27,6 +28,12 @@ const Home = () =>{
             /> 
             : null
             }
+
+            <Grid header='Popular Movies'>
+                {state.results.map(movie=>(
+                    <div key={movie.id}>{movie.title}</div>
+                ))}
+            </Grid>
         
         </>
         
