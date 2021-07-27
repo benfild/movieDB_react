ec7f8bbb95fb8800f2c2d01cbc562d042022291b
benfild <fildben@gmail.com>
2021-07-26 23:48:08 -0400

    adding loadMore btn fetching logic

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -15,6 +15,7 @@ export const useHomeFetch = () => {
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
 
 
     const fetchMovies = async (page, searchTerm="") =>{
@@ -42,5 +43,14 @@ export const useHomeFetch = () => {
         fetchMovies(1, searchTerm);
     }, [searchTerm]);
 
-    return { state, loading, error, setSearchTerm, searchTerm };
+    // Load more
+    useEffect(()=>{
+        if(!isLoadingMore) return;
+
+        fetchMovies(state.page+1, searchTerm);
+        setIsLoadingMore(false);
+
+    }, [isLoadingMore, searchTerm, state.page])
+
+    return { state, loading, error, setSearchTerm, searchTerm, setIsLoadingMore };
 }
