f2e529c633acfcd257e590bd92b3adf786c08c53
benfild <fildben@gmail.com>
2021-07-27 22:42:06 -0400

    adding movie.js page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,23 @@
 import React from 'react';
+//config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 
-const Movie = () => <div>Movie</div>
+//components
+import Grid from './Grid';
+import Spinner from './Spinner';
+
+// Hook
+
+//Image
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+
+    return (
+        <>
+            <div>Movie</div>
+        </>
+    )
+}
 
 export default Movie;
